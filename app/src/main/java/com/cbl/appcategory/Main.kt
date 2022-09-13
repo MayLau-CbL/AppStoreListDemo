dffef5b9b90e7810f3d2d292cb56315f2a19efe6
May Lau <may.lau.dev@gmail.com>
2022-09-14 00:18:48 +0800

    add: main application module

--- app/src/main/java/com/cbl/appcategory/Main.kt
+++ app/src/main/java/com/cbl/appcategory/Main.kt
@@ -5,27 +5,21 @@ import androidx.room.Room
 import com.cbl.appcategory.common.Constants
 import com.cbl.appcategory.data.local.LocalRoomDatabase
 import com.facebook.drawee.backends.pipeline.Fresco
+import dagger.hilt.android.HiltAndroidApp
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
+@HiltAndroidApp
 class Main : Application() {
+    @Inject
     lateinit var retrofit: Retrofit
+    @Inject
     lateinit var db: LocalRoomDatabase
 
 
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this)
-
-        retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        db = Room.databaseBuilder(
-            applicationContext,
-            LocalRoomDatabase::class.java, Constants.DB_NAME
-        )
-            .build()
     }
 }
