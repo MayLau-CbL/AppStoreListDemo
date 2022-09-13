4c13e3c56c65dc7ff9cb8edfa2d7a25b14dcb40b
May Lau <may.lau.dev@gmail.com>
2022-09-14 01:32:50 +0800

    updated: viewmodel inject

--- app/src/main/java/com/cbl/appcategory/ListViewModel.kt
+++ app/src/main/java/com/cbl/appcategory/ListViewModel.kt
@@ -1,15 +1,42 @@
 package com.cbl.appcategory
 
 import android.accounts.NetworkErrorException
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cbl.appcategory.common.Constants
 import com.cbl.appcategory.data.AppDetailInfo
 import com.cbl.appcategory.data.AppInfo
+import com.cbl.appcategory.data.network.AppStoreServerInterface
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ListViewModel(private val repo: ListRepo) : ViewModel() {
+@Module
+@InstallIn(ViewModelComponent::class)
+object ListViewModelModule {
+    @Provides
+    fun providerListRepo(
+        @ApplicationContext context: Context
+    ): ListRepo {
+        val app = context as Main
+        return ListRepo(
+            service = app.retrofit.create(AppStoreServerInterface::class.java),
+            db = app.db,
+            connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        )
+    }
+}
+
+@HiltViewModel
+class ListViewModel @Inject constructor(private val repo: ListRepo) : ViewModel() {
     private companion object {
         const val PAGE_SIZE = 10
         const val TTL_PAGE = 100
@@ -34,6 +61,8 @@ class ListViewModel(private val repo: ListRepo) : ViewModel() {
         isLoadingLiveData.value = false
         isErrorLiveData.value = false
         searchKeyword.value = Constants.EMPTY
+        // refresh once when init finished
+        refresh()
     }
 
     fun refresh() {
--- app/src/main/java/com/cbl/appcategory/Main.kt
+++ app/src/main/java/com/cbl/appcategory/Main.kt
@@ -1,13 +1,10 @@
 package com.cbl.appcategory
 
 import android.app.Application
-import androidx.room.Room
-import com.cbl.appcategory.common.Constants
 import com.cbl.appcategory.data.local.LocalRoomDatabase
 import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
 @HiltAndroidApp
